a54692eb1e08936e47d41cc588a80db7dfa02b70
alexpear <alexmpear@gmail.com>
2026-02-12 16:34:09 -0800

    Data model for generating cartoon faces. Manually written.

--- facegen/src/faceGen.js
+++ facegen/src/faceGen.js
@@ -2,11 +2,69 @@
 
 class Face {
     constructor () {
-        // this.eyeType = 
+        this.randomize();
 
         this.canvas = document.getElementById('faceCanvas')
             .getContext('2d');
         // this.ctx = this.canvas.getContext('2d');
+    }
+
+    randomize () {
+        const PARAM_RANGES = this.paramRanges();
+
+        for (let shapeType in PARAM_RANGES) {
+            
+        }
+
+        // todo
+    }
+
+    paramRanges () {
+        return {
+            hairShape: {
+                bald: {},
+                // LATER more
+            },
+            eyeShape: {
+                u: {
+                    curvature: [0.1, 1],
+                    tilt: [0, 360], // LATER allow pointing upish or downish, but not sideways
+                    // LATER size
+                },
+                v: {
+                    angle: [1, 180],
+                    tilt: [0, 360], // LATER allow pointing upish or downish, but not sideways
+                },
+                lemon: {
+                    tilt: [0, 60],
+                },
+                ellipse: {
+                    tilt: [0, 360],
+                    ellipticity: [0, 1],
+                },
+            },
+            noseShape: {
+                inverted7: {
+                    size: [10, 100],
+                },
+                u: {
+                    size: [10, 100],
+                },
+                w: {
+                    size: [10, 100],
+                },
+            },
+            mouthShape: {
+                curve: {
+                    size: [10, 100],
+                    curvature: [-1, 1],
+                },
+                ellipse: {
+                    size: [10, 100],
+                    ellipticity: [0, 1],
+                },
+            },
+        };
     }
 
     render () {
